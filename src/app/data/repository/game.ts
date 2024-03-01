4f7a77e8e5662bc4a454e5f0d39839f16356bf97
Marcos Eduardo Winiarski <marcoswinrar@gmail.com>
2024-03-01 00:51:20 -0300

    Added new endpoint by game id in repository

---
+++ src/app/data/repository/game.ts
@@ -0,0 +1,8 @@
+import { client } from '@/app/core/axios'
+
+export const getGamesByDate = (date: string) =>
+  client.get('/games', { params: { date } }).then(({ data }) => data)
+
+
+export const getGameById = (id: number) =>
+  client.get('/game', { params: { id } }).then(({data}) => data)
