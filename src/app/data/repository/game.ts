0b3e62d0f27204915633a5dbf46d9fd144562a59
Marcos Eduardo Winiarski <marcoswinrar@gmail.com>
2024-03-02 23:07:29 -0300

    New repository

--- src/app/data/repository/game.ts
+++ src/app/data/repository/game.ts
@@ -1,4 +1,4 @@
-import { client } from '@/app/core/axios'
+import { client } from '../../core/axios'
 
 export const getGamesByDate = (date: string) =>
   client.get('/games', { params: { date } }).then(({ data }) => data)
---
+++ src/app/data/repository/standings.ts
@@ -0,0 +1,4 @@
+import { client } from '../../core/axios'
+
+export const getStandingsByStandardAndCurrentSeason = (season: string) =>
+  client.get('/standings', { params: { league: 'standard', season } }).then(({ data }) => data)
