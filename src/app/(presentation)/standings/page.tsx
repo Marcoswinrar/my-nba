5a6af5ce45d88e228599237a5037460f86078d82
Marcos Eduardo Winiarski <marcoswinrar@gmail.com>
2024-03-03 17:48:58 -0300

    Refactored standings page

--- src/app/(presentation)/standings/page.tsx
+++ src/app/(presentation)/standings/page.tsx
@@ -1,36 +1,14 @@
 import Header from "@/app/components/Header";
-import { getStandingsByStandardAndCurrentSeason } from "@/app/data/repository/standings";
-import { formatStandingsByConference } from "@/app/domain/utils/standingsByConference";
-import { formatTeamsByRanking } from "@/app/domain/utils/teamByRanking";
+import StandingsList from "@/app/components/StandingsList";
 import { standings } from "@/app/mock-data";
 
 const Standings = async () => {
-  const { results, response, errors } = standings  
-  const { east, west } = formatStandingsByConference(response)
+  const { results, response, errors } = standings
 
   return (
     <>
       <Header title="Standings" />
-      <div className="flex w-full gap-7">
-        <section className="east">
-          {formatTeamsByRanking(east)
-            .map(({ team, conference }) => (
-              <div key={team.id}>
-                <span>{team.name}</span>
-                <span>{conference.rank}</span>
-              </div>
-            ))}
-        </section>
-        <section className="west">
-          {formatTeamsByRanking(west)
-            .map(({ team, conference }) => (
-              <div key={team.id}>
-                <span>{team.name}</span>
-                <span>{conference.rank}</span>
-              </div>
-            ))}
-        </section>
-      </div>
+      <StandingsList standings={response}/>
     </>
   )
 }
